59e9b99ab92152597a9d26b9ab2411fb9f54c40d
sudarsan1989 <surendralal@mpie.de>
2020-05-17 00:35:03 +0200

    fixing CI

--- .ci-support/install_tex_packages.sh
+++ .ci-support/install_tex_packages.sh
@@ -24,15 +24,15 @@ fi
 
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
-
-# Other contrib packages: done as a block to avoid multiple calls to tlmgr
-# texlive-latex-base is needed to run pdflatex
-# Take packages from http://ctan.ebinger.cc/tex-archive/systems/texlive/tlnet/archive/
-tlmgr install                    \
-  collection-publishers          \
-  collection-fontsrecommended    \
-  collection-latexextra          \
-  minted                         \
-  hyperref                       \
-  pygmentex                      \
+# tlmgr install luatex
+#
+# # Other contrib packages: done as a block to avoid multiple calls to tlmgr
+# # texlive-latex-base is needed to run pdflatex
+# # Take packages from http://ctan.ebinger.cc/tex-archive/systems/texlive/tlnet/archive/
+# tlmgr install                    \
+#   collection-publishers          \
+#   collection-fontsrecommended    \
+#   collection-latexextra          \
+#   minted                         \
+#   hyperref                       \
+#   pygmentex                      \
