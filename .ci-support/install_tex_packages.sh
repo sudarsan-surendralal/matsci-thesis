163e87f90e84d21293f715df7f756b68ec8756bd
sudarsan1989 <surendralal@mpie.de>
2020-05-17 01:06:22 +0200

    :construction: Installing luatex

--- .ci-support/install_tex_packages.sh
+++ .ci-support/install_tex_packages.sh
@@ -15,25 +15,22 @@ if ! command -v texlua > /dev/null; then
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
   cd install-tl-20*
-  pwd
-
   # Install a minimal system
   ./install-tl --profile=../.ci-support/texlive.profile
-
   cd ..
 fi
 
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
-# tlmgr install luatex
-#
-# # Other contrib packages: done as a block to avoid multiple calls to tlmgr
-# # texlive-latex-base is needed to run pdflatex
-# # Take packages from http://ctan.ebinger.cc/tex-archive/systems/texlive/tlnet/archive/
-# tlmgr install                    \
-#   collection-publishers          \
-#   collection-fontsrecommended    \
-#   collection-latexextra          \
-#   minted                         \
-#   hyperref                       \
-#   pygmentex                      \
+tlmgr install luatex
+
+# Other contrib packages: done as a block to avoid multiple calls to tlmgr
+# texlive-latex-base is needed to run pdflatex
+# Take packages from http://ctan.ebinger.cc/tex-archive/systems/texlive/tlnet/archive/
+tlmgr install                    \
+  collection-publishers          \
+  collection-fontsrecommended    \
+  collection-latexextra          \
+  minted                         \
+  hyperref                       \
+  pygmentex                      \
